39c86f3ac01e864f945f9de16e039e3792a93b4c
Kushagra Raina <kushagra.raina@gmail.com>
2019-08-16 20:13:36 +0530

    updated snakecoin

--- Snakecoin/genesisblock.py
+++ Snakecoin/genesisblock.py
@@ -1,6 +1,7 @@
 import datetime as date
+import snakecoin
 
 def create_genesis_block():
     # Manually construct a block with
     # index zero and arbitrary previous hash
-    return Block(0, date.datetime.now(), "Genesis Block", "0")
+    return Block(1, date.datetime.now(), "Genesis Block", "0")
